f95d97199e0dab2e25ecb0a213b30c927cdba12b
actuallyfro <actuallyfro@github.com>
2021-10-23 20:00:41 -0500

    Adding first gml attempt for archival purposes

---
+++ 00_Examples/00_old_Gml2JSON_uses-too-many-imports/convertGML-to-JSON.py
@@ -0,0 +1,15 @@
+# RUN FIRST!!!!
+# pip install networkx
+# sudo apt install python-pytest
+#OPTIONAL: pip install pygraphviz pydot pyyaml gdal lxml
+
+import networkx as nx
+import simplejson as json
+from networkx.readwrite import json_graph
+
+# parse the gml file and build the graph object
+g = nx.read_gml('dolphins.gml')
+# create a dictionary in a node-link format that is suitable for JSON serialization
+d = json_graph.node_link_data(g)
+with open('dolphins.json', 'w') as fp:
+    json.dump(d, fp)
