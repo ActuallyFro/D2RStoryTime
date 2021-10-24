a17931b4525553d7cfc3b8689e95ce93b7d8204e
actuallyfro <actuallyfro@github.com>
2021-10-23 20:56:02 -0500

    Kinda fixed old version -- it deals with GML ... vs. Neo4J wanting GXL!

--- 00_Examples/00_old_Gml2JSON_uses-too-many-imports/convertGML-to-JSON.py
+++ 00_Examples/00_old_Gml2JSON_uses-too-many-imports/convertGML-to-JSON.py
@@ -3,13 +3,17 @@
 # sudo apt install python-pytest
 #OPTIONAL: pip install pygraphviz pydot pyyaml gdal lxml
 
+#python -c "import networkx"
+
 import networkx as nx
 import simplejson as json
 from networkx.readwrite import json_graph
 
 # parse the gml file and build the graph object
-g = nx.read_gml('dolphins.gml')
+#g = nx.read_gml('dolphins.gml')
+g = nx.read_graphml('other.gxl')
 # create a dictionary in a node-link format that is suitable for JSON serialization
 d = json_graph.node_link_data(g)
-with open('dolphins.json', 'w') as fp:
+#with open('dolphins.json', 'w') as fp:
+with open('other.json', 'w') as fp:
     json.dump(d, fp)
